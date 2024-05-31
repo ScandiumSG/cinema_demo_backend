a22ca7c60ac157ae1a97d328a3bb1a604360817b
Stian Gaustad <stian.gaustad@no.experis.com>
2024-05-31 12:59:24 +0200

    TicketDTO - Removed unused fields

--- cinemaServer/Models/Response/TicketResponse/TicketInScreeningDTO.cs
+++ cinemaServer/Models/Response/TicketResponse/TicketInScreeningDTO.cs
@@ -5,14 +5,6 @@ namespace cinemaServer.Models.Response.TicketResponse
     public class TicketInScreeningDTO
     {
         public int Id { get; set; }
-        
-        public int ScreeningId { get; set; }
-
-        public int MovieId { get; set; }
-
-        public int TheaterId { get; set; }
-
-        public int SeatId { get; set; }
 
         public required SeatIncludedWithTheaterDTO Seat {  get; set; }
 
