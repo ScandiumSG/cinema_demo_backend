49745196c7949990c6e1382e35e71aa1bb952a78
Stian Gaustad <stian.k.gaustad@gmail.com>
2024-10-24 19:48:11 +0200

    Repository - Added highly specific curation repository based on Repository<Movie>

---
+++ cinemaServer/Repository/CurationRepository.cs
@@ -0,0 +1,22 @@
+﻿using cinemaServer.Data;
+using cinemaServer.Models.PureModels;
+using Microsoft.EntityFrameworkCore;
+using System.Transactions;
+
+namespace cinemaServer.Repository
+{
+    public class CurationRepository : Repository<Movie>
+    {
+        public CurationRepository(DataContext context) : base(context)
+        {
+            _context = context;
+            _dbSet = context.Set<Movie>();
+        }
+
+        public async Task<Movie> GetHighlightedMovie() 
+        {
+            // Sort by rating descending, select the first entry (i.e. highest rated movie)
+            return await _dbSet.OrderByDescending(m => m.AverageRating).FirstAsync();
+        }
+    }
+}
